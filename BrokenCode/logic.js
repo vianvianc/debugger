aadc8558bac9eab6d7c990075f5a1167cce8cc41
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-07-15 14:53:14 -0600

    fixed  bugs

--- BrokenCode/logic.js
+++ BrokenCode/logic.js
@@ -1,9 +1,10 @@
-const booFacts = ["Boo is a pomeranian, Boo's best friend is another pomeranian named Buddy, Boo the Pomeranian was born on March 16, making him a Pisces, Boo's favourite food is grass, Boo has released two books" ];
+
 $("#factButton").on("click", function() {
 	const number = Math.floor((Math.random() * booFacts.length));
 	$("#factText").text(booFacts[number])
 })
 
+const booFacts = ["Boo is a pomeranian, Boo's best friend is another pomeranian named Buddy, Boo the Pomeranian was born on March 16, making him a Pisces, Boo's favourite food is grass, Boo has released two books" ]
 
 $("#textPink").on("click", function() {
 	$("#funText").css("color", pink)
